44ab5ac87380c082b5f46a9ab5b9ec48814f73e6
miansheraaz <mianshraaz1@gmail.com>
2022-03-11 17:10:17 +0500

    Check

--- src/app/modules/master/timeline/timeline.routing.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { LeftComponent } from './left/left.component';
-
-
-export const TimelineRoutes: Routes = [
-  {
-    path: '', component: LeftComponent,
-  }
-];
